e24f9aa2c594cf2d05424b926d83ba7f75885805
mikee223 <hmqalora@gmail.com>
2020-04-18 19:37:51 +0800

    commit

--- src/app/routes/masterdata/cmcategory/cmcategory.component.ts
+++ src/app/routes/masterdata/cmcategory/cmcategory.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild, PipeTransform  } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
 import { DatatableComponent } from '@swimlane/ngx-datatable';
 import { DecimalPipe, DatePipe } from '@angular/common';
 
@@ -27,7 +28,17 @@ export class CmcategoryComponent implements OnInit {
   rowsSel = [];
   selected = [];
 
-  columns = [
+   columns = [
+      { prop: 'name' },
+      { name: 'Code' },
+      { name: 'Desc' },
+      { name: 'Count' },
+      { name: 'Category' },
+      { name: 'Rcpt' },
+      { name: 'Reason' }
+   ];
+
+   columnsSort = [
      { prop: 'name' },
      { name: 'Code' },
      { name: 'Desc' },
@@ -35,34 +46,17 @@ export class CmcategoryComponent implements OnInit {
      { name: 'Category' },
      { name: 'Rcpt' },
      { name: 'Reason' }
-  ];
+   ];
 
-  columnsSort = [
-    { prop: 'name' },
-    { name: 'Code' },
-    { name: 'Desc' },
-    { name: 'Cnt' },
-    { name: 'Category' },
-    { name: 'Rcpt' },
-    { name: 'Reason' }
-  ];
 
-  // columns = [
-  //   { prop: 'name' },
-  //   { name: 'Company' },
-  //   { name: 'Gender' }
-  // ];
-  
-  // columnsSort = [
-  //   { prop: 'name' },
-  //   { name: 'Company' },
-  //   { name: 'Gender' }
-  // ];
+  valForm: FormGroup;
+  blackList = ['bad@email.com', 'some@mail.com', 'wrong@email.co'];
+
 
   @ViewChild(DatatableComponent) table: DatatableComponent;
   @ViewChild('myTable') tableExp: any;
 
-  constructor(private npipe: DecimalPipe,private dpipe: DatePipe) {    
+  constructor(private npipe: DecimalPipe, private dpipe: DatePipe, fb: FormBuilder) {    
     this.fetch((data) => {      
       // cache our list      
       this.temp = _clone(data);      
@@ -73,10 +67,80 @@ export class CmcategoryComponent implements OnInit {
       this.rowsSel = _clone(data);      
     });
 
+    let password = new FormControl('', Validators.required);
+    let certainPassword = new FormControl('', CustomValidators.equalTo(password));
+    
+
+    // Model Driven validation
+    this.valForm = fb.group({
+
+      'code': [null, Validators.pattern('^[0-9]+$')],
+      'desc': [null, Validators.required],
+      'count': [null, Validators.pattern('^[0-9]+$')],
+      'category': [null, Validators.pattern('^[0-9]+$')],
+      'receipt': [null, Validators.pattern('^[0-9]+$')],
+      'reason': [],
+
+      // 'sometext': [null, Validators.required],
+      // 'checkbox': [null, Validators.required],
+      // 'radio': ['', Validators.required],
+      // 'select': [null, Validators.required],
+      // 'digits': [null, Validators.pattern('^[0-9]+$')],
+      // 'minlen': [null, Validators.minLength(6)],
+      // 'maxlen': [null, Validators.maxLength(10)],
+      // 'email': [null, CustomValidators.email],
+      // 'url': [null, CustomValidators.url],
+      // 'date': [null, CustomValidators.date],
+      // 'number': [null, Validators.compose([Validators.required, CustomValidators.number])],
+      // 'alphanum': [null, Validators.pattern('^[a-zA-Z0-9]+$')],
+      // 'minvalue': [null, CustomValidators.min(6)],
+      // 'maxvalue': [null, CustomValidators.max(10)],
+      // 'minwords': [null, this.minWords(6)],
+      // 'maxwords': [null, this.maxWords(10)],
+      // 'minmaxlen': [null, CustomValidators.rangeLength([6, 10])],
+      // 'range': [null, CustomValidators.range([10, 20])],
+      // 'rangewords': [null, Validators.compose([this.minWords(6), this.maxWords(10)])],
+      // 'email_bl': [null, this.checkBlackList(this.blackList)],
+
+      // 'passwordGroup': fb.group({
+      //   password: password,
+      //   confirmPassword: certainPassword
+      // })
+    });
+
   }
 
-    ngOnInit() {
+  ngOnInit() {
 
+  }
+
+  submitForm($ev, value: any) {
+    $ev.preventDefault();
+    for (let c in this.valForm.controls) {
+      this.valForm.controls[c].markAsTouched();
+    }
+    if (this.valForm.valid) {
+      console.log('Valid!');
+    }
+    console.log(value);
+  }
+
+  minWords(checkValue): ValidatorFn {
+    return <ValidatorFn>((control: FormControl) => {
+      return (control.value || '').split(' ').length >= checkValue ? null : { 'minWords': control.value };
+    });
+  }
+
+  maxWords(checkValue): ValidatorFn {
+    return <ValidatorFn>((control: FormControl) => {
+      return (control.value || '').split(' ').length <= checkValue ? null : { 'maxWords': control.value };
+    });
+  }
+
+  checkBlackList(list: Array<string>): ValidatorFn {
+    return <ValidatorFn>((control: FormControl) => {
+      return list.indexOf(control.value) < 0 ? null : { 'blacklist': control.value };
+    });
   }
   
 
@@ -148,4 +212,11 @@ export class CmcategoryComponent implements OnInit {
   onActivate(event) {
     console.log('Activate Event', event);
   }
+  
+  getRowClass = (row) => {    
+    return {
+      'row-color1': row.Cnt
+    };
+  }
+
 }
