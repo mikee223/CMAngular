b2ecda3eabb6742526e6d29302b22a909517e1fd
mikee223 <hmqalora@gmail.com>
2020-04-20 23:19:28 +0800

    home

--- src/app/routes/home/home.module.ts
+++ src/app/routes/home/home.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { Routes, RouterModule } from '@angular/router';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { SharedModule } from '../../shared/shared.module';
+import { Ng2TableModule } from 'ng2-table/ng2-table';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -8,7 +11,10 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forChild(routes)
+        SharedModule,        
+        RouterModule.forChild(routes),
+        NgxDatatableModule,
+        Ng2TableModule
     ],
     declarations: [HomeComponent],
     exports: [
--- src/app/routes/home/home/home.component.ts
+++ src/app/routes/home/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, PipeTransform, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn } from '@angular/forms';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
 
 
 @Component({
@@ -6,11 +8,24 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+    @ViewChild(DatatableComponent) tableCount: DatatableComponent;
+
+    columnsCount = [
+        { prop: 'name' },
+        { name: 'Employee' },
+        { name: 'Count' },
+        { name: 'Percentage' }
+    ];
 
     constructor() { }
 
     ngOnInit() {
+    }
+
+    clicked(){
+        console.log('hey')
     }
 
 }
